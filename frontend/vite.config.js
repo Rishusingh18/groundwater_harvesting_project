ab72b1f6593f90a72bbee6e38025c2ff706ce1a6
Rishusingh18 <rishusingh18@example.com>
2025-08-26 14:10:43 -0400

    Update vite.config.js with base path and rebuild frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/groundwater_harvesting_project/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
